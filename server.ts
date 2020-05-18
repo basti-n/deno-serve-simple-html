8fbe02c4b4e097a5cf8d21b75c99259833fc30ac
Sebastian Neumair <sebastianneumair@Sebastians-MBP.fritz.box>
2020-05-18 21:54:03 +0200

    Add Basic Chat functionality

--- server.ts
+++ server.ts
@@ -1,16 +1,28 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
+import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
+import { chat } from "./chat.ts";
 
-const port = 3000;
+const port = 3001;
 
 listenAndServe({ port }, async (req) => {
   if (req.method === "GET" && req.url === "/") {
     const body = await Deno.open("./index.html");
-
-    req.respond({
+    return req.respond({
       status: 200,
       headers: new Headers({ "content-type": "text/html" }),
       body,
     });
+  }
+
+  if(req.method === 'GET' && req.url === '/ws') {
+      if(acceptable(req)) {
+          acceptWebSocket({
+              conn: req.conn,
+              bufWriter: req.w,
+              bufReader: req.r,
+              headers: req.headers
+          }).then(chat)
+      }
   }
 });
 
